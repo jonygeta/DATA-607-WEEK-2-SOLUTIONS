c6e14420d34b052e42078ca95f9dca615d054433
jonygeta <46010980+jonygeta@users.noreply.github.com>
2019-02-10 13:41:50 -0500

    data 607 assignment solved

---
+++ Assignment  SQL and R.sql
@@ -0,0 +1,40 @@
+-- create the table
+
+CREATE TABLE  `movies` (
+  `idmovies` INT NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `rating` INT(1) NOT NULL,
+  `person` VARCHAR(45) NULL);
+
+-- enter the data
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('1', ' The Greatest Showman', '5', '1');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('1', ' The Greatest Showman', '4', '2');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('1', ' The Greatest Showman', '5', '3');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('1', ' The Greatest Showman', '5', '4');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('1', ' The Greatest Showman', '4', '5');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('2', 'A Christmas Story', '5', '1');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('2', 'A Christmas Story', '5', '2');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('2', 'A Christmas Story', '4', '3');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('2', 'A Christmas Story', '3', '4');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('2', 'A Christmas Story', '2', '5');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('3', 'Downsizing', '5', '1');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('3', 'Downsizing', '3', '2');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('3', 'Downsizing', '5', '3');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('3', 'Downsizing', '4', '4');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('3', 'Downsizing', '5', '5');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('4', 'A Christmas Story', '4', '1');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('4', 'A Christmas Story', '5', '2');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('4', 'A Christmas Story', '2', '3');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('4', 'A Christmas Story', '4', '4');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('4', 'A Christmas Story', '5', '5');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('5', 'Coco', '4', '1');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('5', 'Coco', '5', '2');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('5', 'Coco', '3', '3');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('5', 'Coco', '4', '4');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('5', 'Coco', '5', '5');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('6', 'Molly\'s Game', '4', '1');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('6', 'Molly\'s Game', '4', '2');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('6', 'Molly\'s Game', '5', '3');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('6', 'Molly\'s Game', '4', '4');
+INSERT INTO  `movies` (`idmovies`, `title`, `rating`, `person`) VALUES ('6', 'Molly\'s Game', '3', '5');
+
